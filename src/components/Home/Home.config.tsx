7a7d8bd9769e1463cf624cc13885f5dcb1ee6391
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-01-12 15:59:01 -0800

    10. Adds Home Page

---
+++ src/components/Home/Home.config.tsx
@@ -0,0 +1,44 @@
+const styles = {
+    body: {
+        backgroundImage: 'url(https://66.media.tumblr.com/d8ee740f64f90ccb5c909f00e1434941/ad5eba72995ccbaa-4d/s1280x1920/906f1df8790b8dcec4bb4577343fb23755c7eb11.png)',
+        backgroundSize: 'cover',
+    },
+    articleSection: {
+        display: 'flex',
+        maxWidth: '70vw',
+        margin: '3em auto'
+    },
+    articles: {
+        display: 'flex',
+        margin: '0 1.25em',
+        maxWidth: '35%',
+        maxHeight: '275px',
+        padding: '1em',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        backgroundColor: 'whitesmoke',
+        border: '11px solid rgb(170, 90, 70)',
+        borderRadius: '.75em'
+    },
+    articleText: {
+        fontSize:' 1em'
+    },
+    iframe: {
+        display: 'inline-block',
+        padding: 0,
+        margin: 0
+    },
+    video: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: 0,
+        margin: 0
+    },
+    videoDiv: {
+        background: 'rgba(170, 80, 70)',
+        padding: '1em',
+        borderRadius: '1em'
+    }
+}
+
+export default styles
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import styles from './Home.config';
 
 const Home = ({}) => {
     return (
-        <div>
-            Hello from Home
+        <div style={ styles.body }>
+            <div style={ styles.video }>
+                <div style={ styles.videoDiv }>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Sx2UqSxAPfk" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="karuta-video" allowFullScreen></iframe>
+                </div>
+            </div>
+            <section style={ styles.articleSection }>
+                <article style={ styles.articles }>
+                    <h1 style={ styles.articleText }>1st ‘karuta’ world competition held at ‘holy site’ of game in Otsu</h1>
+                    <p>OTSU--After hundreds of years, the traditional Japanese card game of "karuta" has gone global, with a world competition held here for the first time on Nov. 3 with 10 teams participating, including seven from abroad. <a style={{ textDecoration: 'none' }} href="http://www.asahi.com/ajw/articles/AJ201811040026.html"><span style={{ color:'goldenrod' }}> Continue Reading...</span></a></p>
+                </article>
+                <article style={ styles.articles }>
+                    <h1 style={ styles.articleText }>Introducing "Competition Karuta" that became a hot topic in the movie "Chihayafuru"!</h1>
+                    <p>A girl's manga "Chihayufuru", written by manga writer Yuki Suetsugu, is based on a theme of "Competition Karuta". It was also animated and filmed into a movie, and Suzu Hirose who starred the movie has become a hot topic. <a style={{ textDecoration: 'none' }} href="https://www.fun-japan.jp/intl/articles/9561"><span style={{ color:'goldenrod' }}> Continue Reading...</span></a></p>
+                </article>
+                <article style={ styles.articles }>
+                    <h1 style={ styles.articleText }>Karuta: Gotta Catch 'Em All! An old Japanese card game brought into the light</h1>
+                    <p>Nintendo has been a household name in the world over for the last 30 years, and their Pokemon franchise took the world by storm in the 90s. But did you know that the multimedia Nintendo empire started out as a humble karuta company in 1889? <a style={{ textDecoration: 'none' }} href="https://www.tofugu.com/japan/karuta-japanese-cards/"><span style={{ color:'goldenrod' }}> Continue Reading...</span></a></p>
+                </article>
+            </section>
         </div>
     );
 };
