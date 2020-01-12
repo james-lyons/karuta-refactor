de272a1570abbf786a35b8157945ba643025af5c
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-01-12 14:04:33 -0800

    6. Adds constants file, NavBar and Routes folders and files, and login and logout methods for App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import API_URL from './constants';
+import NavBar from './components/NavBar/NavBar';
+import Routes from './routes/Routes';
 
-const App = () => {
-    return (
-        <div>
-            Hello world
-        </div>
-    );
+interface State {
+    currentUser: string,
+    error: null
+};
+
+class App extends React.PureComponent<{}, State> {
+    state: State = {
+        currentUser: '',
+        error: null
+    };
+
+    private setCurrentUser = (userId: string) => {
+        localStorage.setItem('uid', userId)
+        this.setState({ currentUser: userId });
+    };
+
+    private logout = () => {
+        localStorage.removeItem('uid');
+        try {
+            fetch(`${ API_URL }/auth/logout`, { credentials: 'include' })
+        } catch (error) {
+            this.setState({
+                error: error.message
+            });
+        };
+    };
+
+    public render() {
+
+        return (
+            <>
+                <NavBar />
+                <Routes />
+            </>
+        );
+    }
 };
 
 export default App;
